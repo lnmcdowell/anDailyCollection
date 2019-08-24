5c93054e8d2512d427462e423b375ed9bf79743a
nathanielmcdowell <nathaniel29651@gmail.com>
2019-08-24 13:40:43 -0400

    hmm

--- anDailyCollection/SFCMapViewController.swift
+++ anDailyCollection/SFCMapViewController.swift
@@ -22,63 +22,114 @@ class SFCMapViewController: UIViewController {
         self.view.frame = UIScreen.main.bounds
         
         self.view.addSubview(myScrollView)
+          myScrollView.stickToContainer(container: self.view)
+        myScrollView.heightAnchor.constraint(equalTo: self.view.heightAnchor).isActive = true
+        let greenView = UIStackView()
+        greenView.axis = .horizontal
+        greenView.alignment = .top
+        greenView.spacing = 0
+        greenView.distribution = .fillProportionally
+        
+        greenView.backgroundColor = UIColor.green
+        
+        myScrollView.addSubview(greenView)
+        
+        greenView.translatesAutoresizingMaskIntoConstraints = false
+        greenView.leadingAnchor.constraint(equalTo: myScrollView.leadingAnchor).isActive = true
+        greenView.trailingAnchor.constraint(equalTo: myScrollView.trailingAnchor).isActive = true
+        greenView.topAnchor.constraint(equalTo: myScrollView.topAnchor).isActive = true
+        greenView.bottomAnchor.constraint(equalTo: myScrollView.bottomAnchor).isActive = true
+        greenView.heightAnchor.constraint(equalTo: myScrollView.heightAnchor).isActive = true
+        greenView.widthAnchor.constraint(equalToConstant: 4500).isActive = true
         
         
-        let stackView = UIStackView()
-        stackView.axis = .horizontal
-        stackView.alignment = .fill
-        stackView.spacing = 0
-        stackView.distribution = .fill
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        myScrollView.addSubview(stackView)
+        
+        for i in 0...8 {
+        let label = UILabel()
+        label.backgroundColor = .orange
+        label.text = "I'm label \(i)."
+        label.textAlignment = .center
+        greenView.addArrangedSubview(label)
+        }
+        //return
+
         
         
-        myScrollView.stickToContainer(container: self.view)
-        stackView.stickToContainer(container: myScrollView)
         
-        myScrollView.isScrollEnabled = true
-        myScrollView.showsHorizontalScrollIndicator = true
-        myScrollView.clipsToBounds = false
-        //myScrollView.contentSize.width = 1200
         
-        //self.view.heightAnchor.constraint(equalToConstant: UIScreen.main.bounds.height).isActive = true
         
-        stackView.addSubview(SFCImageView)
-        stackView.addSubview(SFC2ndImageView)
-        stackView.addSubview(SFC3rdImageView)
         
-        stackView.heightAnchor.constraint(equalTo: myScrollView.heightAnchor).isActive = true
-        stackView.widthAnchor.constraint(equalToConstant: 1500).isActive = true
         
-        SFCImageView.translatesAutoresizingMaskIntoConstraints = false
+//        let stackView = UIStackView()
+//        stackView.axis = .horizontal
+//        stackView.alignment = .fill
+//        stackView.spacing = 0
+//        stackView.distribution = .fill
+//        stackView.translatesAutoresizingMaskIntoConstraints = false
+//        myScrollView.addSubview(stackView)
+//
+        
+//        myScrollView.stickToContainer(container: self.view)
+//        stackView.stickToContainer(container: myScrollView)
+//
+//        myScrollView.isScrollEnabled = true
+//        myScrollView.showsHorizontalScrollIndicator = true
+//        myScrollView.clipsToBounds = false
+
+//        stackView.axis = .horizontal
+        greenView.addArrangedSubview(SFCImageView)
+        greenView.addArrangedSubview(SFC2ndImageView)
+        //greenView.addArrangedSubview(SFC3rdImageView)
+        //
+ 
+//        stackView.heightAnchor.constraint(equalTo: myScrollView.heightAnchor).isActive = true
+//        stackView.widthAnchor.constraint(equalToConstant: 1500).isActive = true
+//
+        
+        
+//        SFCImageView.translatesAutoresizingMaskIntoConstraints = false
+//        SFCImageView.widthAnchor.constraint(equalToConstant: 400).isActive = true
+//        SFCImageView.heightAnchor.constraint(equalToConstant: 400).isActive = true
+//        SFC2ndImageView.translatesAutoresizingMaskIntoConstraints = false
+//        SFC2ndImageView.widthAnchor.constraint(equalToConstant: 400).isActive = true
+//        SFC2ndImageView.heightAnchor.constraint(equalToConstant: 400).isActive = true
+//        SFC3rdImageView.translatesAutoresizingMaskIntoConstraints = false
+//        SFC3rdImageView.widthAnchor.constraint(equalToConstant: 400).isActive = true
+//        SFC3rdImageView.heightAnchor.constraint(equalToConstant: 400).isActive = true
+//
+        
        // SFCImageView.centerYAnchor.constraint(equalTo: stackView.centerYAnchor, constant: 0).isActive = true
        // SFCImageView.centerXAnchor.constraint(equalTo: stackView.centerXAnchor, constant: 0).isActive = true
-        SFCImageView.doConstraints(top: stackView.topAnchor, leading: stackView.leadingAnchor, bottom: nil, trailing: SFC2ndImageView.leadingAnchor, topPad: 0, leadPad: 0, botPad: 0, trailPad: 0, width: 400, height: 400)
-  
-   
-        SFC2ndImageView.doConstraints(top: SFCImageView.topAnchor, leading: SFCImageView.trailingAnchor, bottom:nil, trailing: SFC3rdImageView.leadingAnchor, topPad: 5, leadPad: 0, botPad: 0, trailPad: 0, width: 400, height: 400)
-         SFC3rdImageView.doConstraints(top: SFCImageView.topAnchor, leading: SFC2ndImageView.trailingAnchor, bottom: nil, trailing: nil, topPad: 5, leadPad: 0, botPad: 0, trailPad: 0, width: 400, height: 400)
+//       SFCImageView.doConstraints(top: stackView.topAnchor, leading: stackView.leadingAnchor, bottom: nil, trailing: nil, topPad: 0, leadPad: 0, botPad: 0, trailPad: 0, width: 400, height:400)
+////
+////
+//        SFC2ndImageView.doConstraints(top: SFCImageView.topAnchor, leading: SFCImageView.trailingAnchor, bottom:nil, trailing: nil, topPad: 0, leadPad: 0, botPad: 0, trailPad: 0, width: 400, height: 400)
+//         SFC3rdImageView.doConstraints(top: SFCImageView.topAnchor, leading: SFC2ndImageView.trailingAnchor, bottom: nil, trailing: nil, topPad: 0, leadPad: 0, botPad: 0, trailPad: 0, width: 400, height: 400)
         //make this look a little shorter to simulate 3d depth around center image
+       // myScrollView.contentSize.width = myScrollView.frame.width * 3
         
     }
     
     var SFCImageView:UIImageView = {
        let iv = UIImageView()
         iv.contentMode = .scaleAspectFit
+
         iv.imageFromUrl(urlString: "https://www.aviationweather.gov/data/products/progs/F000_wpc_sfc.gif", me: iv)
         return iv
     }()
     
     var SFC2ndImageView:UIImageView = {
-        let iv = UIImageView()
-        iv.contentMode = .scaleAspectFit
-        iv.imageFromUrl(urlString: "https://www.aviationweather.gov/data/products/progs/F006_wpc_sfc.gif", me: iv)
-        return iv
+        let av = UIImageView()
+        av.contentMode = .scaleAspectFit
+ 
+        av.imageFromUrl(urlString: "https://www.aviationweather.gov/data/products/progs/F006_wpc_sfc.gif", me: av)
+        return av
     }()
     
     var SFC3rdImageView:UIImageView = {
         let iv = UIImageView()
         iv.contentMode = .scaleAspectFit
+ 
         iv.imageFromUrl(urlString: "https://www.aviationweather.gov/data/products/progs/F006_wpc_sfc.gif", me: iv)
         return iv
     }()
